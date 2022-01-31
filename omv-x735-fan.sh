38f593a0cdbff93a5ac1b2d77851266b0ee08995
Alex Link <alexlink809@gmail.com>
2022-01-30 23:31:10 -0500

    boot script LSB compliant

--- omv-x735-fan.sh
+++ omv-x735-fan.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+### BEGIN INIT INFO
+# Provides:          omv-x735-fan.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+# Carry out specific functions when asked to by the system
 case "$1" in
   start)
     echo "Starting omv-x735-fan.py"
