9559b7582ae4c3054d3ad316f11e75111cd3570b
Alex Link <alexlink809@gmail.com>
2022-01-30 21:42:28 -0500

    gpio mode set to BCM.  logging and pwm setting more efficient

--- omv-x735-fan.py
+++ omv-x735-fan.py
@@ -13,12 +13,11 @@ DEBUG_LOGGING = False
 def setup():
     if DEBUG_LOGGING: print('Setting up GPIO...')
     global pwm
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_PIN, GPIO.OUT)
     GPIO.output(GPIO_PIN, GPIO.LOW)
     pwm = GPIO.PWM(GPIO_PIN, 25000)
     pwm.start(0)
-    if DEBUG_LOGGING: print('GPIO setup complete.')
 
 def get_temp():
     output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
@@ -29,27 +28,28 @@ def get_temp():
         raise RuntimeError('Could not parse temperature output.')
 
 def loop():
+    temp = 0
     while True:
+        prevTemp = temp
         temp = get_temp()
         if DEBUG_LOGGING: print('Core temperature:', temp)
 
-        if temp > 70:
-            if DEBUG_LOGGING: print('temp > 70, setting fan to 100%')
+        if temp >= 70 and prevTemp < 70:
+            if DEBUG_LOGGING: print('temp >= 70, setting fan speed to 100%')
             pwm.ChangeDutyCycle(100)
-        elif temp > 60:
-            if DEBUG_LOGGING: print('temp > 60, setting fan to 75%')
+        elif temp >= 60 and (prevTemp < 60 or prevTemp >= 70):
+            if DEBUG_LOGGING: print('temp >= 60, setting fan speed to 80%')
             pwm.ChangeDutyCycle(80)
-        elif temp > 50:
-            if DEBUG_LOGGING: print('temp > 50, setting fan to 50%')
+        elif temp >= 50 and (prevTemp < 50 or prevTemp >= 60):
+            if DEBUG_LOGGING: print('temp >= 50, setting fan speed to 60%')
             pwm.ChangeDutyCycle(60)
-        elif temp > 40:
-            if DEBUG_LOGGING: print('temp > 40, setting fan to 25%')
+        elif temp >= 40 and (prevTemp < 40 or prevTemp >= 50):
+            if DEBUG_LOGGING: print('temp >= 40, setting fan speed to 40%')
             pwm.ChangeDutyCycle(40)
-        elif temp > 30:
-            if DEBUG_LOGGING: print('temp > 30, setting fan to 10%')
+        elif temp >= 30 and (prevTemp < 30 or prevTemp >= 40):
+            if DEBUG_LOGGING: print('temp >= 30, setting fan speed to 20%')
             pwm.ChangeDutyCycle(20)
         else:
-            if DEBUG_LOGGING: print('temp < 30, setting fan to 0%')
             pwm.ChangeDutyCycle(0)
 
         time.sleep(SLEEP_INTERVAL)
@@ -62,6 +62,7 @@ def destroy():
 
 if __name__ == '__main__':
     setup()
+    if DEBUG_LOGGING: print('GPIO setup complete.  Starting main loop...')
     try:
         loop()
     except KeyboardInterrupt:
